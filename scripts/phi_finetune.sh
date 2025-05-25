7615bfd57f26a628ebd288ff9401832f6a620ead
lati672 <1297091773@qq.com>
2025-05-25 12:31:16 +0000

    modify the finetuning script

---
+++ scripts/phi_finetune.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Dynamically assign a free port
+export MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()")
+
+# Define model and split combinations
+model_name=phi-1_5
+forget_retain_splits=(
+    "forget01 retain99"
+    #"forget05 retain95"
+    #"forget10 retain90"
+    )
+
+    # Loop over splits
+    for split in "${forget_retain_splits[@]}"; do
+	    forget_split=$(echo $split | cut -d' ' -f1)
+            retain_split=$(echo $split | cut -d' ' -f2)
+	    task_name=tofu_${model_name}_${retain_split}
+
+		echo "Running $task_name..."
+		echo "$forget_split"
+		echo "$retain_split"
+		CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file configs/accelerate/default_config.yaml --main_process_port $MASTER_PORT \
+		src/train.py \
+		--config-name=train.yaml \
+		experiment=finetune/tofu/default \
+		model=${model_name} \
+		task_name=${task_name} \
+		data.forget_split=${forget_split} \
+		data.retain_split=${retain_split}
+	done
+
